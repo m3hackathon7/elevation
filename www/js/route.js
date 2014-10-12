77e5c730ce11188e5c154fc1bda516a656a39f60
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2014-10-13 01:49:28 +0900

    Show axis.

--- www/js/route.js
+++ www/js/route.js
@@ -244,6 +244,39 @@ angular.module('starter.route', [])
         }
       });
 
+      var margin = {
+        top: 20,
+        right: 20,
+        bottom: 30,
+        left: 50
+      };
+      var width = element.prop('offsetWidth') - margin.left - margin.right;
+      var height = 300 - margin.top - margin.bottom;
+      $log.debug(width, height);
+
+      var x = d3.scale.linear()
+        .range([0, width]);
+      var y = d3.scale.linear()
+        .range([height, 0]);
+
+      var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient('bottom');
+      var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient('left');
+
+      var area = d3.svg.area()
+        .x(function(d) { return x(d.distance); })
+        .y0(height)
+        .y1(function(d) { return y(d.elevation); });
+
+      var svg = d3.select(element[0]).append('svg')
+        .attr('width', width + margin.right + margin.left)
+        .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
       function prepareData(route) {
         var points = [];
         var distance = 0;
@@ -267,32 +300,6 @@ angular.module('starter.route', [])
       }
 
       function showRoute(data) {
-        var width = element.prop('offsetWidth');
-        var height = 300;
-        $log.debug(width, height);
-
-        var x = d3.scale.linear()
-          .range([0, width]);
-        var y = d3.scale.linear()
-          .range([height, 0]);
-
-        var xAxis = d3.svg.axis()
-          .scale(x)
-          .orient('bottom');
-        var yAxis = d3.svg.axis()
-          .scale(y)
-          .orient('left');
-
-        var area = d3.svg.area()
-          .x(function(d) { return x(d.distance); })
-          .y0(height)
-          .y1(function(d) { return y(d.elevation); });
-
-        var svg = d3.select(element[0]).append('svg')
-          .attr('width', width)
-          .attr('height', height)
-        .append('g');
-
         x.domain([0, d3.max(data, function(d) { return d.distance; })]);
         y.domain([0, d3.max(data, function(d) { return d.elevation; })]);
 
@@ -306,7 +313,13 @@ angular.module('starter.route', [])
           .call(xAxis);
         svg.append('g')
           .attr('class', 'elevation-chart-axis elevation-chart-axis-y')
-          .call(yAxis);
+          .call(yAxis)
+        .append('text')
+          .attr('transform', 'rotate(-90)')
+          .attr('y', 6)
+          .attr('dy', '.71em')
+          .style('text-anchor', 'end')
+          .text('Elevation (m)');
       }
     }
   };
