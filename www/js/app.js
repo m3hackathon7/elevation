985ea519017b9db391fb45edbcbc7cfd8fef2b54
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2014-10-24 14:01:49 +0900

    Enable Google Maps Autocomplete!

--- www/js/app.js
+++ www/js/app.js
@@ -89,10 +89,11 @@ angular.module('elevation', [
   self.setCurrentLocation = function(fromOrTo) {
     Location.currentPosition()
       .then(function(position) {
-        if (fromOrTo == 'from') {
-          self.from = position.latitude + ',' + position.longitude;
-        } else if (fromOrTo == 'to') {
-          self.to = position.latitude + ',' + position.longitude;
+        var latlng = [position.latitude, position.longitude].join(',');
+        if (fromOrTo === 'from') {
+          self.from = latlng;
+        } else if (fromOrTo === 'to') {
+          self.to = latlng;
         }
       });
   };
